0a37fe3e455b43a5de917a75857e05924cff8984
vjimcor955 <vjimcor955@g.educaand.es>
2024-11-10 16:08:23 +0100

    Mensaje despliegue home

--- Puesta producción segura/Infraestructura desarrollo web/nextjs-boilerplate/app/page.tsx
+++ Puesta producción segura/Infraestructura desarrollo web/nextjs-boilerplate/app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import HomeComponent from "./components/Home";
 
 export default function Home() {
   return (
@@ -47,6 +50,9 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+        <div>
+          <HomeComponent />
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
