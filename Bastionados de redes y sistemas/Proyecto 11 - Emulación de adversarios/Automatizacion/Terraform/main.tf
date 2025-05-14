26fcdb1c41bd69a3c848d38f9f1ebc8646d907a0
Nicolas <144184381+SataNico04@users.noreply.github.com>
2025-05-14 14:07:06 +0200

    Update main.tf

--- Bastionados de redes y sistemas/Proyecto 11 - Emulación de adversarios/Automatizacion/Terraform/main.tf
+++ Bastionados de redes y sistemas/Proyecto 11 - Emulación de adversarios/Automatizacion/Terraform/main.tf
@@ -2,21 +2,96 @@ provider "virtualbox" {
 
 }
 
-resource "virtualbox_vm" "proyecto11_vm" {
-  name   = "proyecto11"
-  image  = var.vm_iso_path
-  cpus   = 2
-  memory = 2048
+# Sección de las redes.
+# Vamos a crear 3 redes
+
+# Red 1 10.10.10.0/24
+resource "virtualbox_hostonly" "red_10" {
+  name       = "red10"
+  ip_address = "10.10.10.1"
+  netmask    = "255.255.255.0"
+}
+
+# Red 2 10.10.20.0/24
+resource "virtualbox_hostonly" "red_20" {
+  name       = "red20"
+  ip_address = "10.10.20.1"
+  netmask    = "255.255.255.0"
+}
+
+# Red 3 10.10.30.0/24
+resource "virtualbox_hostonly" "red_30" {
+  name       = "red30"
+  ip_address = "10.10.30.1"
+  netmask    = "255.255.255.0"
+}
+
+# Sección de las redes.
+# PC1 - red1 + NAT 
+resource "virtualbox_vm" "pc1" {
+  name   = "pc1"
+  image  = "ubuntu.box" 
 
   network_adapter {
-    type           = "bridged"
-    host_interface = var.host_interface  # Ej: "enp3s0" o "eth0"
+    type           = "hostonly"
+    host_interface = virtualbox_hostonly.red_10.name
   }
 
-  ssh_username = var.ssh_user
-  ssh_password = var.ssh_password
-
-  # Si usas disco base en lugar de ISO:
-  # disk_path = var.disk_path
+  network_adapter {
+    type = "nat"
+  }
 }
-Vo
+
+# PC2 - red1 y red2
+resource "virtualbox_vm" "pc2" {
+  name  = "pc2"
+  image = "ubuntu.box"
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = virtualbox_hostonly.red_10.name
+  }
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = virtualbox_hostonly.red_20.name
+  }
+}
+
+# PC3 - red2
+resource "virtualbox_vm" "pc3" {
+  name  = "pc3"
+  image = "ubuntu.box"
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = virtualbox_hostonly.red_20.name
+  }
+}
+
+# PC4 - red1 y red3
+resource "virtualbox_vm" "pc4" {
+  name  = "pc4"
+  image = "ubuntu.box"
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = virtualbox_hostonly.red_10.name
+  }
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = virtualbox_hostonly.red_30.name
+  }
+}
+
+# PC5 - red3
+resource "virtualbox_vm" "pc5" {
+  name  = "pc5"
+  image = "ubuntu.box"
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = virtualbox_hostonly.red_30.name
+  }
+}
